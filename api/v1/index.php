bcf31e185ad7ab762b426384f009e3161ff1d674
cleDiego <cleversondesign@hotmail.com>
2022-01-29 17:42:32 -0300

    create CORS function

--- api/v1/index.php
+++ api/v1/index.php
@@ -8,6 +8,10 @@ use Slim\Factory\AppFactory;
 $app = AppFactory::create();
 $app->setBasePath('/Bank/api/v1');
 
+//Enabled CORS
+$cors = require __DIR__ . '/App/CORS.php';
+$cors($app);
+
 //Error Slim Middleware
 $errorMiddleware = $app->addErrorMiddleware(false, false, false);
 
