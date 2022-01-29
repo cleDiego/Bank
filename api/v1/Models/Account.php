b47128008f87ed96efc4f5e619f12af84072a297
cleDiego <cleversondesign@hotmail.com>
2022-01-29 18:05:11 -0300

    create Model Account

--- api/v1/Models/Account.php
+++ api/v1/Models/Account.php
@@ -1,6 +1,47 @@
 <?php
 namespace Bank\Models;
 
-class Account {
+class Account
+{
+
+    private $id;
+    private $amount;
+    private $balance;
+
+    public function __construct($id = null, $amount = 0)
+    {
+        $this->id = $id;
+        $this->amount = $amount;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function geAmount()
+    {
+        return $this->amount;
+    }
+
+    public function setAmount($amount = 0)
+    {
+        $this->amount = $amount;
+    }
+
+    public function getBalance()
+    {
+        return $this->balance;
+    }
+
+    public function setBalance($balance = 0)
+    {
+        $this->balance = $balance;
+    }
 
 }
